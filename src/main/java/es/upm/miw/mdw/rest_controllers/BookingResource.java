aa5ee04f138cd009e8360b892222da9f613ab979
Bruno Martin <=>
2019-04-26 23:23:09 +0200

    isValidTimeForHabitacion added to BookingResource and its tests

--- src/main/java/es/upm/miw/mdw/rest_controllers/BookingResource.java
+++ src/main/java/es/upm/miw/mdw/rest_controllers/BookingResource.java
@@ -1,11 +1,13 @@
 package es.upm.miw.mdw.rest_controllers;
 
+import es.upm.miw.mdw.controllers.HabitacionController;
 import es.upm.miw.mdw.dtos.ReservaDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @PreAuthorize("permitAll()")
 @RestController
@@ -16,9 +18,23 @@ public class BookingResource {
 
     public static final String SAVE = "/room";
 
+    public static final String VALIDATION = "/{codigoHabitacion}";
+
+    @Autowired
+    HabitacionController habitacionController;
+
     @PostMapping(value = SAVE)
-    public ReservaDto save(@RequestBody ReservaDto dto){
+    public ReservaDto save(@RequestBody ReservaDto dto) {
         System.out.println("Se llama a mi endpoint");
         return dto;
     }
+
+    @GetMapping(value = SAVE + VALIDATION)
+    public void isValidTimeForHabitacion(@PathVariable String codigoHabitacion, @RequestParam String fechaHoraReservaInicio,
+                                         @RequestParam String fechaHoraReservaFin) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        habitacionController.isValidTimeForHabitacion(codigoHabitacion, LocalDateTime.parse(fechaHoraReservaInicio, formatter),
+                LocalDateTime.parse(fechaHoraReservaFin, formatter));
+    }
+
 }
